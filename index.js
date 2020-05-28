1e33b298d2b3145bdf79f91bc66be81fd4313019
Angelica <Angelica137@users.noreply.github.com>
2020-05-28 18:21:54 +0100

    create front end

--- index.js
+++ index.js
@@ -9,17 +9,12 @@ server.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public");
+  res.sendFile(__dirname + "/public/");
 });
 
-//set up socket
-//somone connects
 io.on("connection", (socket) => {
-  //alert in the console what is happening
   console.log("user connected");
-  //we then emit a message on that event
   socket.emit("message", { manny: "hey how are you?" });
-  //we wait for another event from the client and whatever data is passed we print it
   socket.on("another event", (data) => {
     console.log(data);
   });
